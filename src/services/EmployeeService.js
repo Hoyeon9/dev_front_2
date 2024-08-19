7247f95f2afb0719a4805181c9790b127ab05f5a
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-19 11:05:41 +0900

    check url

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 const REST_API_BASE_URL =  import.meta.env.VITE_APIURL;
 
 const REST_API_URL = `${REST_API_BASE_URL}/employees`
+console.log(REST_API_URL)
 //'http://localhost:8080/api/employees';
 
 //export const listEmployees = () => axios.get(REST_API_BASE_URL);
